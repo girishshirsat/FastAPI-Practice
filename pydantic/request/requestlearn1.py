08be90f82cdc0d13a3d706c3da67e19990e3b31a
girishshirsat <girishshirsath2004@gmail.com>
2026-04-14 17:22:41 +0530

    added new endpoint to show data

--- pydantic/request/requestlearn1.py
+++ pydantic/request/requestlearn1.py
@@ -4,11 +4,28 @@ from typing import List
 
 app = FastAPI()
 
+
+
 class Item(BaseModel):
     ID: int
     name: str
     price: float
 
+
+itemlist:List[Item]=[]
+
 @app.post("/items")
-def createitems(items: List[Item]):  # ← List of items
+def createitems(items: list[Item]):
+    itemlist.clear()
+    itemlist.extend(items)  # ← List of items
     return items
+
+
+# from requestlearn2 import responce
+
+
+
+@app.get("/itemlist")
+def get_itemlist():
+    return itemlist
+
--- pydantic/request/requestlearn2.py
+++ pydantic/request/requestlearn2.py
@@ -1,5 +1,6 @@
 import requests
 
+
 Items=[
     {"ID":1,"name":"Bag","price":300.00},
     {"ID":2,"name":"Laptop","price":300000.00},
@@ -9,3 +10,4 @@ Items=[
 
 responce=requests.post("http://127.0.0.1:8000/items",json=Items)
 print(responce.json())
+
